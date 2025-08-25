e5ee6d5bca945090c74dfcadb64d5e6027dcefd5
serigne khadim mbacke ba <serignekhadimmbackeba40@gmail.com>
2025-08-25 17:26:50 +0000

    generations des models  a la bd

--- backend/objets/admin.py
+++ backend/objets/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from backend.objets.models import Declaration, Objet, Restitution
+
 # Register your models here.
+admin.site.register(Objet)
+admin.site.register(Declaration)
+admin.site.register(Restitution)
--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from backend.users.models import Commissariat, Utilisateur
+
 # Register your models here.
+admin.site.register(Utilisateur)
+admin.site.register(Commissariat)
